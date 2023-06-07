1bacc0cc7aaeb1005309b22e38649c68bdcb85e2
PC-DOMENICO\dcarb <d.carbonemsn00@libero.it>
2023-06-07 04:05:40 +0200

    Login con google abilitato (possiamo anche personalizzare il testo (in questo caso ho messo "Continua con Google") ♥

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
-import {signInWithEmailAndPassword} from "firebase/auth"
+import {signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider} from "firebase/auth"
 import auth from "../../firebase";
+import { GoogleButton } from 'react-google-button';
 import {Link, useNavigate} from "react-router-dom";
 
 export default function SignIn() {
@@ -19,6 +20,17 @@ export default function SignIn() {
         })
     }
 
+    const handleSignInWithGoogle = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then((userCredential) => {
+                console.log(userCredential)
+                navigate("/")
+            }).catch(err => {
+                console.log(err)
+        })
+    }
+
     return (
         <div className="formContainer">
             <div className='formWrapper'>
@@ -27,6 +39,7 @@ export default function SignIn() {
                     <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}/>
                     <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
                     <button type="submit">Login</button>
+                    <GoogleButton label='Continua con Google' onClick={handleSignInWithGoogle} />
                     <p>Don't you have an account? <Link to="/register">Register</Link></p>
                 </form>
             </div>
