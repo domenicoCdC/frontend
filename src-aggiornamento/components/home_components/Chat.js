72a583b743557a70de3453cda7652eabf824f8e0
francescodep1 <rino.depietro59@gmail.com>
2023-06-08 19:43:20 +0200

    guardare la cartella src-aggiornamento, tutto con codice commentato, per non creare eventuali errori

---
+++ src-aggiornamento/components/home_components/Chat.js
@@ -0,0 +1,18 @@
+{/*import React, {useContext} from 'react'
+import Messages from "./Messages";
+import Input from "./Input";
+import {ChatContext} from "../../context/ChatContext";
+
+const Chat =()=>{
+    const{data}= useContext(ChatContext);
+    return (
+        <div className='chat'>
+            <div className="chatInfo">
+                <span> {data.user?.firstName} </span>
+            </div>
+            <Messages/>
+            <Input/>
+        </div>
+    )
+}
+export default Chat*/}
---
+++ src-aggiornamento/components/home_components/NavBar.js
@@ -0,0 +1,21 @@
+{/*import {signOut} from "firebase/auth";
+import auth from "../../firebase";
+//import {AuthContext} from "../context/AuthContext";
+//import React, {useContext} from 'react'
+
+const NavBar =()=>{
+    //const {currentUser}=useContext(AuthContext)
+    return (
+        <div className='navbar'>
+        <div className="user">
+
+            <button onClick={()=>signOut(auth)}>logout</button>
+        </div>
+        </div>
+    )
+}
+export default NavBar*/}
+
+/*<span>{currentUser.displayName}</span>
+* NEL CASO DI AGGIUNTA DEL NOME NELLA REGISTRAZIONE
+* */
