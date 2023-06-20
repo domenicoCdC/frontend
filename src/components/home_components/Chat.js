d11301f094c960b709dd71be2e35d0b8a8891cdb
christianp12 <krys.petruz@yahoo.it>
2023-06-20 22:49:19 +0200

    Divisione task

--- src/components/home_components/Chat.js
+++ src/components/home_components/Chat.js
@@ -11,17 +11,15 @@ const Chat =()=>{
     const sendRequestHandler = () => {
         //DEVE FARE CHIAMATA API A http://localhost:3001/api/friend-requests/ NEL
         /**
-         * TODO: nel corpo della post, bisogna mettci fromUserId: l'id dell'utente attualmente loggato (che lo posso prelevare dal AuthContext
-         * TODO: e toUserId:(che lo posso prelevare da ChatContext) data.user.uid (da verificare..vedi Chats.js
-         * TODO: modificare il backend perchè è presente la caratterizzazione dell'id tramite username che noi abbiamo tolto
-         * TODO: e le varie api relative a friend-requests; (idea: l'id della richiesta d'amicizia può essere il combineid del from user e to user
-         * TODO: idea: se tutto va a buon fine far comparire un bottone di successo sostituendo quello di default di "add request" con "richiesta d'amcizia già inviata"
+         * Christian: nel corpo della post, bisogna mettci fromUserId: l'id dell'utente attualmente loggato (che lo posso prelevare dal AuthContext
+         * Christian: e toUserId:(che lo posso prelevare da ChatContext) data.user.uid (da verificare..vedi Chats.js
+         * Christian: modificare il backend perchè è presente la caratterizzazione dell'id tramite username che noi abbiamo tolto
+         * Christian: e le varie api relative a friend-requests; (idea: l'id della richiesta d'amicizia può essere il combineid del from user e to user
+         * Christian: idea: se tutto va a buon fine far comparire un bottone di successo sostituendo quello di default di "add request" con "richiesta d'amcizia già inviata"
          * TODO: GESTIRE inoltre la sezione riguardante il profilo per visuliazzare lo stato delle richieste d'amicizia
-         * TODO: discutere su dove mettere la sezione PROFILO idea: mettere un avatar (img circolare di default = per tutti)
-         * TODO: al click dell'avatar mostrare : fistNAme + last name, email: .... , un bottone per reimpostare psw, e richieste d'amicizia con la possibilità di vedere lo stato ("pending" = inviata, ...)
-         *
+         * Francesco: discutere su dove mettere la sezione PROFILO idea: mettere un avatar (img circolare di default = per tutti)
+         * Domenico: pagina profilo: al click dell'avatar mostrare : fistNAme + last name, email: .... , un bottone per reimpostare psw, e richieste d'amicizia con la possibilità di vedere lo stato ("pending" = inviata, ...)
          */
-
     }
 
     return (
